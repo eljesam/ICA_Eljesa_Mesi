11754ef6e84feb3b904a5ca7a227049562f7f48e
eljesam <124920260+eljesam@users.noreply.github.com>
2025-01-12 17:17:34 +0000

    configured pubnub in app.py and connected pubnub to the database

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from app.hardware.hardware_control import initialize_hardware, read_sensor_data, control_acutator
+from app.pubnub_config import pubnub
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/vision_control'
@@ -29,3 +30,22 @@ def contact():
 if __name__ == '__main__':
     initialize_hardware()
     app.run(debug=True)
+    
+    
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory
+
+class MySubscribeCallback(SubscribeCallback):
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNConnectedCategory:
+            pubnub.publish().channel('vision_control').message('Hello from the other side').sync()
+    def message(self, pubnub, message):
+        pass
+    
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels('vision_control').execute()
+
+@app.route('/publish')
+def publish_message():
+    pubnub.publish().channel('vision_control').message('Hello from the other side').sync()
+    return 'Message published'
